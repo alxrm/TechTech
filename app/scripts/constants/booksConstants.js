ce72e3ce342252817397bd3bef7bb63ff50e7c89
alex <rupta3@gmail.com>
2017-04-20 12:25:03 +0300

    Fixed images paths

--- app/scripts/constants/booksConstants.js
+++ app/scripts/constants/booksConstants.js
@@ -40,73 +40,73 @@ const booksSlides = [
 
 const books = [
   {
-    image: '../images/book-6.jpg',
+    image: 'images/book-6.jpg',
     name: 'AntiPatterns: Refactoring Software, Architectures, and Projects in Crisis',
     author: 'William J. Brown',
     link: 'https://vk.cc/6xj7ji'
   },
   {
-    image: '../images/book-7.jpg',
+    image: 'images/book-7.jpg',
     name: 'Writing Effective Use Cases',
     author: 'Alistair Cockburn',
     link: 'https://vk.cc/6xjaRq'
   },
   {
-    image: '../images/book-8.jpg',
+    image: 'images/book-8.jpg',
     name: 'UML Distilled: A Brief Guide to the Standard Object Modeling Language',
     author: 'Martin Fowler',
     link: 'https://vk.cc/6xjbY6'
   },
   {
-    image: '../images/book-9.jpg',
+    image: 'images/book-9.jpg',
     name: 'Manage It!: Your Guide to Modern, Pragmatic Project Management',
     author: 'Johanna Rothman',
     link: 'https://vk.cc/6xjd6k'
   },
   {
-    image: '../images/book-10.jpg',
+    image: 'images/book-10.jpg',
     name: 'Agile and Iterative Development: A Manager\'s Guide',
     author: 'Craig Larman',
     link: 'https://vk.cc/6xjdXB'
   },
   {
-    image: '../images/book-11.jpg',
+    image: 'images/book-11.jpg',
     name: 'Object-Oriented Software Construction',
     author: 'Bertrand Meyer',
     link: 'https://vk.cc/6xjeqk'
   },
   {
-    image: '../images/book-12.jpg',
+    image: 'images/book-12.jpg',
     name: 'Writing Solid Code',
     author: 'Steve Maguire',
     link: 'https://vk.cc/6xjfik'
   },
   {
-    image: '../images/book-1.jpg',
+    image: 'images/book-1.jpg',
     name: 'Управление проектами по созданию ПО',
     author: 'Уокер Ройс',
     link: 'https://vk.cc/6xeOGR'
   },
   {
-    image: '../images/book-2.jpg',
+    image: 'images/book-2.jpg',
     name: 'Практика программирования',
     author: 'Брайан Керниган',
     link: 'https://vk.cc/6xeOxF'
   },
   {
-    image: '../images/book-3.jpg',
+    image: 'images/book-3.jpg',
     name: 'Быстрая разработка',
     author: 'Стив Макконнелл',
     link: 'https://vk.cc/6xeOQ9'
   },
   {
-    image: '../images/book-4.jpg',
+    image: 'images/book-4.jpg',
     name: 'Мастерство программирования',
     author: 'Пит Макбрин',
     link: 'https://vk.cc/6xeOWS'
   },
   {
-    image: '../images/book-5.jpg',
+    image: 'images/book-5.jpg',
     name: 'Унифицированный процесс разработки',
     author: 'Ивар Якобсон',
     link: 'https://vk.cc/6xeP1I'
